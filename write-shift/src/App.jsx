509b091eb2bccc3de1056e8548e8de8b809e8dcb
Krishneshvar <krishjn07@gmail.com>
2024-10-13 20:51:33 +0530

    Components have been destructured.

--- write-shift/src/App.jsx
+++ write-shift/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Home from './components/Home/Home';
 
 function App() {
     const currYear = new Date().getFullYear();
@@ -6,35 +7,13 @@ function App() {
   return (
     <main>
       <nav>
-        <div>
-          <h3>WriteShift</h3>
-        </div>
+        <h3> WriteShift </h3>
       </nav>
 
-      <section className='video-sect'>
-        <video autoPlay loop muted playsInline>
-          <source src="/Written-Text1.mp4" type="video/mp4" />
-        </video>
-      </section>
-
-      <section className="content-sect">
-        <div className="content-wrapper">
-          <div className="upload-area">
-            <input type="file" className="hidden" />
-            <p>Drag and drop a file here, or click to select a file</p>
-          </div>
-
-          <div className="text-display">
-            <h2>Generated Text</h2>
-            <p className="output-text">.</p>
-          </div>
-        </div>
-      </section>
+      <Home />
 
       <footer>
-        <div>
-          <p>&copy; {currYear} WriteShift. All rights reserved.</p>
-        </div>
+        &copy; {currYear} WriteShift. All rights reserved.
       </footer>
     </main>
   )
--- write-shift/src/components/Home/Home.jsx
+++ write-shift/src/components/Home/Home.jsx
@@ -3,7 +3,25 @@ import './Home.css'
 function Home() {
     return(
         <>
-        <video src="/Written-Text.mp4" className='video' autoPlay loop/>
+        <section className='video-sect'>
+            <video autoPlay loop muted playsInline>
+                <source src="/Written-Text.mp4" type="video/mp4" />
+            </video>
+        </section>
+
+        <section className="content-sect">
+            <div className="content-wrapper">
+                <div className="upload-area">
+                    <input type="file" className='file-selector' />
+                    <p> Drag and drop a file here, or click to select a file </p>
+                </div>
+
+                <div className="text-display">
+                    <h2> Generated Text </h2>
+                    <p className="output-text"></p>
+                </div>
+            </div>
+        </section>
         </>
     )
 }
